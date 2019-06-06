741c46ef9cdd8c84a3377896a5253291f04a31ea
Sumit Saini <sumit.saini@nagarro.com>
2019-06-06 17:03:15 +0530

    jhfj

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace acidemo.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "lets", "go" };
         }
 
         // GET api/values/5
