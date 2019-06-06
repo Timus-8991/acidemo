7766461a680cac857554c428a91b24e6df738b4e
Sumit Saini <sumit.saini@nagarro.com>
2019-06-06 17:53:05 +0530

    testing one again

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace acidemo.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "have", "fun" };
+            return new string[] { "have", "fun with me" };
         }
 
         // GET api/values/5
